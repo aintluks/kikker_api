76f673c1604a0a0c7177d6eb89b44c3b97c8f8b4
Lucas Costa <aintluks@gmail.com>
2025-04-18 10:59:40 -0300

    Add posts controller create route

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,29 @@
+module Api::V1
+  class PostsController < ApplicationController
+    before_action :set_user, only: [:create]
+
+    def create
+      post = @user.posts.build(post_params)
+
+      if post.save
+        render json: post, status: :created
+      else
+        render_validation_errors(post)
+      end
+    end
+
+    private
+
+    def set_user
+      @user = User.find_or_create_by!(login: params[:login])
+    end
+
+    def render_validation_errors(invalid_record)
+      render json: { errors: invalid_record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def post_params
+      params.permit(:title, :body, :ip)
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    before_action :ensure_json_request
+
+    private
+
+    def ensure_json_request
+        return if request.format == :json
+        render status: :not_acceptable, json: { error: 'Only JSON requests are allowed' }
+    end
 end
