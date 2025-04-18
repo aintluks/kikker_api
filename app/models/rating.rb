4bebf0a91db5f373b767e9fc63e56df6a3dfe925
Lucas Costa <aintluks@gmail.com>
2025-04-18 12:25:11 -0300

    Add average rating for rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,12 +2,17 @@ class Rating < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  validates :value, presence: true, 
-                    numericality: { 
+  validates :value, presence: true,
+                    numericality: {
                       only_integer: true,
                       greater_than_or_equal_to: 1,
-                      less_than_or_equal_to: 5 
+                      less_than_or_equal_to: 5
                     }
 
   validates :user_id, uniqueness: { scope: :post_id, message: "can only rate a post once" }
+
+  def self.average_rating(post_id)
+    post = Post.find(post_id)
+    post.ratings.average(:value)
+  end
 end
